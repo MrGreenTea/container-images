11415caf9d939ebf29c8b7e6972209246640e59a
Jonas Bulik <jonas@bulik.dev>
2025-06-09 21:41:13 +0200

    More improvements

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-podman build --pull -t localhost/fedora-toolbox:39 toolbox/fedora/
+#!/usr/bin/env sh
+FEDORA_VERSION=42
+
+podman build --pull -t localhost/fedora-toolbox:${FEDORA_VERSION} --build-arg VERSION=${FEDORA_VERSION} toolbox/fedora/
 podman build --pull -t localhost/ubuntu-toolbox:22.04 toolbox/ubuntu/
